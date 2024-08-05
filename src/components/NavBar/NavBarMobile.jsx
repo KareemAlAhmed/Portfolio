4f4785361ad39b63a6cf73245f5e4a26a89b6a12
KareemAlAhmed <karimahmad2172@gmail.com>
2024-08-05 12:03:37 +0300

    Fixing MobileNavBAr

--- src/components/NavBar/NavBarMobile.jsx
+++ src/components/NavBar/NavBarMobile.jsx
@@ -55,11 +55,10 @@ useEffect(()=>{ document.querySelectorAll(".menu__btn").forEach(dropDownFunc)},[
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
       </a>
       <ul className="menu__list">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#about" onClick={closeDropdown}>About</a></li>
+        <li><a href="#projects" onClick={closeDropdown}>Projects</a></li>
+        <li><a href="#resume" onClick={closeDropdown}>Resume</a></li>
+        <li><a href="#contact" onClick={closeDropdown}>Contact</a></li>
       </ul>
     </div>
   </div>
